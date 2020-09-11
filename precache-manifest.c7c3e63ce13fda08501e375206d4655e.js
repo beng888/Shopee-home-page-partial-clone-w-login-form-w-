fcac4dbe94ffccadbd1212f3f13982c321980279
lawrence ardosa <lawrenceardosa@gmail.com>
2020-09-11 17:13:56 +0800

    Updates

---
+++ precache-manifest.c7c3e63ce13fda08501e375206d4655e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f67d698293486278c587cf42766e4588",
+    "url": "/Shopee-home-page-partial-clone-w-login-form-w-/index.html"
+  },
+  {
+    "revision": "69ac5a926826fac95398",
+    "url": "/Shopee-home-page-partial-clone-w-login-form-w-/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "38d00ecf82f3efb1bbb0",
+    "url": "/Shopee-home-page-partial-clone-w-login-form-w-/static/css/main.f8511d6d.chunk.css"
+  },
+  {
+    "revision": "69ac5a926826fac95398",
+    "url": "/Shopee-home-page-partial-clone-w-login-form-w-/static/js/2.e5d7d797.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/Shopee-home-page-partial-clone-w-login-form-w-/static/js/2.e5d7d797.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38d00ecf82f3efb1bbb0",
+    "url": "/Shopee-home-page-partial-clone-w-login-form-w-/static/js/main.4f426750.chunk.js"
+  },
+  {
+    "revision": "872158792d9a3d3bacdc",
+    "url": "/Shopee-home-page-partial-clone-w-login-form-w-/static/js/runtime-main.aa139606.js"
+  }
+]);
